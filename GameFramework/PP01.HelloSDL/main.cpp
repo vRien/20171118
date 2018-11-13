ad0f4726d25021a569b3b4975371a77577a10242
dafher801 <dafher801@gmail.com>
2018-11-13 12:56:12 +0900

    no message

--- GameFramework/PP01.HelloSDL/main.cpp
+++ GameFramework/PP01.HelloSDL/main.cpp
@@ -1,30 +1,49 @@
 #include<SDL.h>
 
-SDL_Window* g_pWindow = 0;
-SDL_Renderer* g_pRenderer = 0;
+SDL_Window*g_pWindow = 0;
+SDL_Renderer*g_pRenderer = 0;
 
-int main(int argc, char* args[])
-{
+bool g_bRunning = false;
 
+bool init(const char* title, int xpos, int ypos, int width, int height, int flags) {
 	if (SDL_Init(SDL_INIT_EVERYTHING) >= 0)
 	{
+		g_pWindow = SDL_CreateWindow(title, xpos, ypos, width, height, flags);
 
-		g_pWindow = SDL_CreateWindow("PP01.HelloSDL",
-			SDL_WINDOWPOS_CENTERED,
-			SDL_WINDOWPOS_CENTERED,
-			640, 480, SDL_WINDOW_SHOWN);
-		if (g_pWindow != 0) {
+		if (g_pWindow != 0)
+		{
 			g_pRenderer = SDL_CreateRenderer(g_pWindow, -1, 0);
 		}
 	}
 	else {
+		return false;
+	}
+	return true;
+}
+void render()
+{
+	SDL_SetRenderDrawColor(g_pRenderer, 0, 0, 0, 255);
+
+	SDL_RenderClear(g_pRenderer);
+
+	SDL_RenderPresent(g_pRenderer);
+}
+void update() {}
+void handleEvents() {}
+void clean() {}
+
+int main(int argc, char* argv[])
+{
+	if (init("Chapter 1 : Setting up SDL", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_SHOWN)) {
+		g_bRunning = true;
+	}
+	else {
 		return 1;
 	}
+	while (g_bRunning) {
+		render();
+	}
 
-	SDL_SetRenderDrawColor(g_pRenderer, 0, 0, 0, 255);
-	SDL_RenderClear(g_pRenderer);
-	SDL_RenderPresent(g_pRenderer);
-	SDL_Delay(5000);
 	SDL_Quit();
 	return 0;
 }
