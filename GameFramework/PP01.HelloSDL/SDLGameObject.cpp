c6730c38a9ce6808a2710b1da93917b80338adba
kuna3844 <kuna3844@naver.com>
2018-11-16 02:38:39 +0900

    PP12.Vector2D

--- GameFramework/PP01.HelloSDL/SDLGameObject.cpp
+++ GameFramework/PP01.HelloSDL/SDLGameObject.cpp
@@ -2,13 +2,10 @@
 #include "TextureManager.h"
 #include "Game.h"
 
-SDLGameObject::SDLGameObject(const LoaderParams* pParams) : GameObject(pParams)
+SDLGameObject::SDLGameObject(const LoaderParams* pParams) : GameObject(pParams), m_position(pParams->getX(), pParams->getY()), m_velocity(0.0f, 0.0f), m_acceleration(0.0f, 0.0f)
 {
-	m_x = pParams->getX();
-	m_y = pParams->getY();
 	m_width = pParams->getWidth();
 	m_height = pParams->getHeight();
-
 	m_textureID = pParams->getTextureID();
 	m_currentRow = 1;
 	m_currentFrame = 1;
@@ -16,7 +13,15 @@ SDLGameObject::SDLGameObject(const LoaderParams* pParams) : GameObject(pParams)
 
 void SDLGameObject::draw()
 {
-	TextureManager::Instance()->drawFrame(m_textureID, m_x, m_y,
+	TextureManager::Instance()->drawFrame(m_textureID,
+		(int)m_position.getX(), (int)m_position.getY(),
 		m_width, m_height, m_currentRow, m_currentFrame,
 		TheGame::Instance()->getRenderer());
 }
+
+void SDLGameObject::update()
+{
+	m_velocity += m_acceleration;
+	m_position += m_velocity;
+}
+
--- GameFramework/PP01.HelloSDL/SDLGameObject.h
+++ GameFramework/PP01.HelloSDL/SDLGameObject.h
@@ -1,19 +1,22 @@
 #pragma once
 #include "GameObject.h"
+#include "Vector2D.h"
 
 class SDLGameObject : public GameObject
 {
 public:
 	SDLGameObject(const LoaderParams* pParams);
 	virtual void draw();
-	virtual void update() {};
+	virtual void update();
 	virtual void clean() {};
 protected:
-	int m_x;
-	int m_y;
 	int m_width;
 	int m_height;
 	int m_currentRow;
 	int m_currentFrame;
-	std::string m_textureID;
+	std::string m_textureID; 
+	Vector2D m_position;
+	Vector2D m_velocity;
+	Vector2D m_acceleration;
+
 };
